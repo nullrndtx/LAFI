5777961d56a3da5850b704db104d422e8935e3d1
nullrndtx <rndtx@gros-team.org>
2015-08-05 00:11:26 +0700

    Update scripts

--- installer.sh
+++ installer.sh
@@ -5,3 +5,4 @@ sudo cp 70-android-tools-fastboot.rules /lib/udev/rules.d
 sudo cp adb.1.gz /usr/share/man/man1
 sudo cp adb /usr/bin
 sudo cp fastboot /usr/bin
+zenity --info --title "LAFI" --text "Installation Complete, Thanks for using LAFI"
--- uninstaller.sh
+++ uninstaller.sh
@@ -4,4 +4,4 @@ sudo rm /lib/udev/rules.d/70-android-tools-fastboot.rules
 sudo rm /usr/share/man/man1/adb.1.gz 
 sudo rm /usr/bin/adb 
 sudo rm /usr/bin/fastboot 
-
+zenity --info --title "LAFI" --text "All Files Removed"
